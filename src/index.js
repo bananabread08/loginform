40f9b557d925c153a0ad934fdd6d1b624a40a8ae
bananabread08 <rmkfermin@gmail.com>
2021-03-09 14:09:27 +0800

    added remaining manual validations.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const zipcodeEl = document.getElementById('zip-code');
 const passwordEl = document.getElementById('password');
 const passwordCheckEl = document.getElementById('password-check');
 const form = document.getElementById('form');
-// const signin = document.getElementById('sign-in');
 
 const profileFactory = (email, country, zipcode, password, passwordCheck) => ({
   email, country, zipcode, password, passwordCheck,
@@ -27,40 +26,67 @@ const showSucessFor = (input) => {
 // validation
 const validateEmail = (email) => {
   if (email === '') {
-    showErrorFor(emailEl, 'Email Address is blank. Try again.');
+    showErrorFor(emailEl, 'Email Address is required and cannot be blank.');
   } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
     showErrorFor(emailEl, 'Please submit a valid Email.');
   } else showSucessFor(emailEl);
 };
 const validateCountry = (country) => {
   if (country === '') {
-    showErrorFor(countryEl, 'Country is blank. Try again');
+    showErrorFor(countryEl, 'Country is required and cannot be blank.');
   } else {
     showSucessFor(countryEl);
   }
 };
 const validateZipCode = (zipcode) => {
-  if (zipcode) showSucessFor(zipcodeEl);
+  const regex = /^\[0-9]{5}[-\s]?(?:[0-9]{4})?$/gm;
+  if (!regex.test(zipcode) && (zipcode.length !== 9) && (zipcode !== '')) {
+    showErrorFor(zipcodeEl, 'Invalid Zip Code. Please input a 9 digit Number.');
+  } else showSucessFor(zipcodeEl);
+};
+const validatePassword = (password) => {
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm;
+  if (password === '') showErrorFor(passwordEl, 'Password is required and cannot be blank.');
+  else if (!regex.test(password)) showErrorFor(passwordEl, 'Password must be 8 characters long, at least one letter, one number and one special character:');
+  else showSucessFor(passwordEl);
 };
 
-const validatePassword = (password, passwordCheck) => {
-  if (password === '') showErrorFor(passwordEl, 'Password is blank. Try again');
-  else showSucessFor(passwordEl);
-  if (passwordCheck === '') showErrorFor(passwordCheckEl, 'Confirm Password is blank. Try again');
-  else if (password !== passwordCheck) showErrorFor(passwordCheckEl, 'Passwords do not match');
-  else showSucessFor(passwordCheckEl);
+const validateInput = (event) => {
+  const inputId = event.target.id;
+  const inputValue = event.target.value;
+
+  // eslint-disable-next-line default-case
+  switch (inputId) {
+    case 'email':
+      validateEmail(inputValue);
+      break;
+    case 'country':
+      validateCountry(inputValue);
+      break;
+    case 'zip-code':
+      validateZipCode(inputValue);
+      break;
+    case 'password':
+      validatePassword(inputValue);
+  }
 };
 
 const getAndCheckInputs = () => {
   const newProfile = profileFactory(emailEl.value, countryEl.value,
     zipcodeEl.value, passwordEl.value, passwordCheckEl.value);
-  validateEmail(newProfile.email);
-  validateCountry(newProfile.country);
-  validateZipCode(newProfile.zipcode);
-  validatePassword(newProfile.password, newProfile.passwordCheck);
+  return newProfile;
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   getAndCheckInputs();
+
+  if (emailEl.parentElement.className === 'form-input success'
+  && countryEl.parentElement.className === 'form-input success'
+  && zipcodeEl.parentElement.className === 'form-input success'
+  && passwordEl.parentElement.className === 'form-input success') alert('Valid!');
+});
+
+form.addEventListener('input', (e) => {
+  validateInput(e);
 });
